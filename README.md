6fd96a6f25b9a2421d832bc2bc4cbe23b1edfb14
Creabine <creabine@foxmail.com>
2017-01-11 14:49:20 +0800

    publish

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# Creabine-Carousel
+# Creabine Carousel
+## 介绍
 一款简单的渐变轮播插件
+## 怎么使用
+1 引用文件
+
+`<link rel="stylesheet" href="Creabine-Carousel.css">`
+
+`<script src="Creabine-Carousel.js"></script>`
+    
+2 添加标签
+
+`<div id="carouselRoot"></div>`
+
+3 js调用
+`var Carousel=new CreabineCarousel(
+    {
+        root:"carouselRoot",
+        images:[url1,url2,url3,url4,url5,...],
+        contant:[{title:"title1",text:"text1"},{title:"title2",text:"text2"},...],
+        autoScroll:true
+    }
+)`
+
+
+
+### Options
+| name          | type         | default         | required   |
+| --------      | ---------    |:----------:     | ----------:| 
+| root          | string       | NaN             |  yes       |
+| images        | Array    	   | NaN             |  yes       |
+| contant       | Object array | NaN             |  yes       |
+| autoScroll    | boolean      | false           |  no        |
+| scrollDuration| number       | 5000            |  no        |
+| height	    | number       | 700             |  no        |
+
+
+
---
+++ src/Creabine-Carousel.js
@@ -0,0 +1,108 @@
+/**
+ * Created by Creabine on 17/01/11.
+ */
+
+
+/**
+ *初始化配置
+ * @param options
+ */
+
+function CreabineCarousel(options){
+	var imgPathList = options.images;
+    var textList = options.contant;
+    if (!options.root) {
+        throw "require root to this CreabineCarousel";
+    }
+    if (!imgPathList) {
+        throw "must provide parameter images";
+    }
+    if (imgPathList.length != textList.length) {
+        throw "images are not equal to contants";
+    }
+    var changeCount = 0;
+    var timer;
+    var _autoScroll = options.autoScroll || false;
+    var _scrollDuration = options.scrollDuration || 4000;
+    var _height = options.height || 700;
+
+    function initElements() {
+    	var _root = document.getElementById(options.root);
+    	if (!_root) {
+            throw "no exist called this name element,please create element called this name";
+        }
+        _root.className = "CreabineCarousel";
+        _root.style.height = _height + "px";
+        var _dotContainer = document.createElement("ul");
+        _dotContainer.className = 'CreabineCarousel-dotContainer';
+        _root.appendChild(_dotContainer);
+        for (var i = 0; i < imgPathList.length; i++) {
+            var _dot = document.createElement("li");
+            _dot.className = "dot";
+            _dot.id = "item" + (i+1) + "dot";
+            _dotContainer.appendChild(_dot);
+            var _item = document.createElement("div");
+            _item.className = "CreabineCarousel-item"
+            _item.id = "item" + (i+1);
+            _item.style.backgroundImage = "url(" + imgPathList[i] + ")";
+            _item.style.backgroundSize = "cover";
+            _item.style.backgroundRepeat = "no-repeat";
+            if(i == 0){
+                _item.style.opacity = '0';
+                _item.style.zIndex = '1';
+            }
+            _root.appendChild(_item);
+            var _h = document.createElement("h1");
+            _h.innerText = textList[i].title;
+            _item.appendChild(_h);
+            var _p = document.createElement("p");
+            _p.innerText = textList[i].text;
+            _item.appendChild(_p);
+        }
+        _dotContainer.addEventListener("mouseover",function(e){
+        	if( e.target && e.target.className == "dot" ){
+        		clearInterval(timer);
+        		var id = e.target.id.substring(0,5);
+        		CarouselHover(id);
+        	}
+        });
+        _dotContainer.addEventListener("mouseout",function(e){
+        	if( e.target && e.target.className == "dot" ){
+        		var id = e.target.id;
+        		CarouselOut(id);
+        	}
+        });
+        if(_autoScroll){
+            timer = setInterval(function(){Carousel()},_scrollDuration);
+        }
+
+    }
+    function Carousel(){
+        var all = document.getElementsByClassName('CreabineCarousel-item');
+        for (var i = all.length - 1; i >= 0; i--) {
+            all[i].style.opacity = '0';
+            all[i].style.zIndex = '1';
+        }
+        var i=((changeCount++%5)+1);
+        var id = "item" + i;
+        document.getElementById(id).style.opacity = '1';
+        document.getElementById(id).style.zIndex = '10';
+    }
+    function CarouselHover(id){
+        clearInterval(timer);
+        var all = document.getElementsByClassName('CreabineCarousel-item');
+        for (var i = all.length - 1; i >= 0; i--) {
+            all[i].style.opacity = '0';
+            all[i].style.zIndex = '1';
+        }
+        document.getElementById(id).style.opacity = '1';
+        document.getElementById(id).style.zIndex = '10';
+    }
+    function CarouselOut(id){
+        var num = id.substring(4,5);
+        num = parseInt(num)-1;
+        changeCount = num;
+        timer = window.setInterval(function(){Carousel()},_scrollDuration);
+    }
+    initElements();
+}
